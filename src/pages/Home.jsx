4eeee1c4b789ad3d3869533eccff35e47e9fd887
Sakib <sahadothosain6619@gmail.com>
2024-05-15 14:53:10 +0600

    added hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Nav from "../components/header/Nav";
+import Hero from "../components/hero/Hero";
 const Home = () => {
   return (
     <div>
       <Nav />
+      <Hero />
     </div>
   );
 };
